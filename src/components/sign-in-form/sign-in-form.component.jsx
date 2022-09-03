43a8ddda76dd8ebaab411ca96785e62938e8107c
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2022-09-03 21:39:41 +0530

    used styled component feature inside navigation component and added button styled component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
-
 import {
-  googleSignInStart,
-  emailSignInStart,
-} from '../../store/user/user.action';
+  signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-in-form.styles.scss';
 
 const defaultFormFields = {
   email: '',
@@ -17,7 +16,6 @@ const defaultFormFields = {
 };
 
 const SignInForm = () => {
-  const dispatch = useDispatch();
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
@@ -26,14 +24,14 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    dispatch(googleSignInStart());
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      dispatch(emailSignInStart(email, password));
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (error) {
       console.log('user sign in failed', error);
@@ -47,7 +45,7 @@ const SignInForm = () => {
   };
 
   return (
-    <SignInContainer>
+    <div className="sign-in-container">
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -68,7 +66,7 @@ const SignInForm = () => {
           name="password"
           value={password}
         />
-        <ButtonsContainer>
+        <div className="buttons-container">
           <Button type="submit">Sign In</Button>
           <Button
             buttonType={BUTTON_TYPE_CLASSES.google}
@@ -77,10 +75,96 @@ const SignInForm = () => {
           >
             Sign In With Google
           </Button>
-        </ButtonsContainer>
+        </div>
       </form>
-    </SignInContainer>
+    </div>
   );
 };
 
 export default SignInForm;
+// import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+
+// import FormInput from '../form-input/form-input.component';
+// import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+// import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
+
+// import {
+//   googleSignInStart,
+//   emailSignInStart,
+// } from '../../store/user/user.action';
+
+// const defaultFormFields = {
+//   email: '',
+//   password: '',
+// };
+
+// const SignInForm = () => {
+//   const dispatch = useDispatch();
+//   const [formFields, setFormFields] = useState(defaultFormFields);
+//   const { email, password } = formFields;
+
+//   const resetFormFields = () => {
+//     setFormFields(defaultFormFields);
+//   };
+
+//   const signInWithGoogle = async () => {
+//     dispatch(googleSignInStart());
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       dispatch(emailSignInStart(email, password));
+//       resetFormFields();
+//     } catch (error) {
+//       console.log('user sign in failed', error);
+//     }
+//   };
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setFormFields({ ...formFields, [name]: value });
+//   };
+
+//   return (
+//     <SignInContainer>
+//       <h2>Already have an account?</h2>
+//       <span>Sign in with your email and password</span>
+//       <form onSubmit={handleSubmit}>
+//         <FormInput
+//           label="Email"
+//           type="email"
+//           required
+//           onChange={handleChange}
+//           name="email"
+//           value={email}
+//         />
+
+//         <FormInput
+//           label="Password"
+//           type="password"
+//           required
+//           onChange={handleChange}
+//           name="password"
+//           value={password}
+//         />
+//         <ButtonsContainer>
+//           <Button type="submit">Sign In</Button>
+//           <Button
+//             buttonType={BUTTON_TYPE_CLASSES.google}
+//             type="button"
+//             onClick={signInWithGoogle}
+//           >
+//             Sign In With Google
+//           </Button>
+//         </ButtonsContainer>
+//       </form>
+//     </SignInContainer>
+//   );
+// };
+
+// export default SignInForm;
